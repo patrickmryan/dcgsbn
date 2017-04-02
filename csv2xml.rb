c8126037337569b83b2257c5e5a9760985b39181
Patrick M. Ryan <pmryan@us.ibm.com>
2017-04-02 15:42:24 -0400

    creating more sub-elements

--- csv2xml.rb
+++ csv2xml.rb
@@ -10,5 +10,21 @@ equip = Nokogiri::XML::Element.new("Equipment",doc)
 doc.add_child(equip)
 
 equip.add_child("<Id>abcdefg</Id>")
+#puts "equip class = " + equip.class.to_s + "\n"
+
+secinfo = Nokogiri::XML::Element.new("SecurityInfo",doc)
+#puts "secinfo class = " + secinfo.class.to_s + "\n"
+
+equip.add_child(secinfo)
+
+#secinfo.add_child("<ClassificationInfo>")
+
+sec_tags = [ "ClassificationInfo", "DeclassificationInfo", "OwnerProducer", "SecurityID" ]
+  
+sec_tags.each do |tag|
+  secinfo.add_child("<#{tag}>")
+  
+end
+
 
 puts doc.to_s
