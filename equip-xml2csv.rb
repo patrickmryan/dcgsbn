2e48a88487750e67cfa31ba91be9c8cda7320dd1
Patrick M. Ryan <pmryan@us.ibm.com>
2017-04-03 16:34:35 -0400

    more refactoring

--- equip-xml2csv.rb
+++ equip-xml2csv.rb
@@ -50,28 +50,29 @@ entity_name = 'Equipment'
 ARGV.each do | file |
   doc = File.open(file) { |f| Nokogiri::XML(f) }
 
-  doc.css(entity_name).each do |node|
+  #doc.css(entity_name).each do |node|
+  node = doc.at_css(entity_name)
 
-    row_hash = Hash.new('')
+  row_hash = Hash.new('')
 
-    # extract the standard info
-    standard_columns.each do | name |
-      set = doc.xpath("/#{entity_name}/#{name}")
+  # extract the standard info
+  standard_columns.each do | name |
+    set = doc.xpath("/#{entity_name}/#{name}")
 
-      if set.length > 0
-        node = set.first
-        value = node.content()
-      else
-        value = ''
-      end
-      row_hash[name] = value
-
+    if set.length > 0
+      node = set.first
+      value = node.content()
+    else
+      value = ''
     end
-
-    # finally, write out the collected data as a row to the csv file
-    csv << all_headers.collect { | key | row_hash[key] }
+    row_hash[name] = value
 
   end
+
+  # finally, write out the collected data as a row to the csv file
+  csv << all_headers.collect { | key | row_hash[key] }
+
+  #end
 
 end
 
--- unit-xml2csv.rb
+++ unit-xml2csv.rb
@@ -61,57 +61,64 @@ entity_name = 'Unit'
 # iterate over the files on the command line
 ARGV.each do | file |
   doc = File.open(file) { |f| Nokogiri::XML(f) }
-    
-  doc.css(entity_name).each do |node|
 
-    row_hash = Hash.new('')
+  #doc.css(entity_name).each do |node|
+  node = doc.at_css(entity_name)
 
-    # extract the standard info
-    standard_columns.each do | name |
-      set = doc.xpath("/#{entity_name}/#{name}")
+  row_hash = Hash.new('')
 
-      if set.length > 0
-        node = set.first
-        value = node.content()
-      else
-        value = ''
-      end
-      row_hash[name] = value
+  # extract the standard info
+  standard_columns.each do | name |
+    set = doc.xpath("/#{entity_name}/#{name}")
 
+    if set.length > 0
+      node = set.first
+      value = node.content()
+    else
+      value = ''
     end
-
-    # now get specific properties
-    nodeset = doc.css('PropertyValues')
-    property_values = nodeset.first.elements
-
-    prop_columns.each do | key |
-
-      value = nil
-      property_values.each do |val|
-        name = val.elements.find { |e | e.name() == 'PropertyName' }
-        if (name.content() == key)
-          value = val.elements.find { |e | e.name() == 'Value' }
-        end
-      end
-
-      row_hash[key] = (value ? value : '') # use space if property not found
-
-    end
-
-    # now get lat/lon
-
-    locations = doc.xpath("//Unit//Locations")
-    loc = locations.first.elements.first
-    location_columns.each do |key|
-      prop = loc.elements.find { | e | e.name() == key }
-
-      row_hash[key] = (prop ? prop.content() : '')  # handle case of missing field
-    end
-
-    # finally, write out the collected data as a row to the csv file
-    csv << all_headers.collect { | key | row_hash[key] }
+    row_hash[name] = value
 
   end
+
+  # now get specific properties
+  nodeset = doc.css('PropertyValues')
+  property_values = nodeset.first.elements
+
+  prop_columns.each do | key |
+
+    value = nil
+    property_values.each do |val|
+      name = val.elements.find { |e | e.name() == 'PropertyName' }
+      if (name.content() == key)
+        value = val.elements.find { |e | e.name() == 'Value' }
+      end
+    end
+
+    row_hash[key] = (value ? value : '') # use space if property not found
+
+  end
+
+  # now get lat/lon
+  nodeset = doc.xpath("/#{entity_name}/Locations/Location")
+
+  if (!nodeset.empty?)
+    loc = nodeset.first
+
+    location_columns.each do |key|
+      prop = loc.elements.find { | e | e.name() == key }
+      row_hash[key] = (prop ? prop.content() : '')  # handle case of missing field
+    end
+  end
+
+  # fix unit Number
+  k = 'Number'
+  row_hash[k] = "'" + row_hash[k]  # prepend a single quote to force Excel to import at text
+
+  # finally, write out the collected data as a row to the csv file
+  csv << all_headers.collect { | key | row_hash[key] }
+
+  #end
 
 end
 
