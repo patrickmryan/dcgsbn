81ea536efecbcdb425d394d66e8430a391457f8c
Patrick M. Ryan <pmryan@us.ibm.com>
2017-04-03 20:43:47 -0400

    rewriting for stdin

--- importxml.rb
+++ importxml.rb
@@ -5,26 +5,22 @@ end
 
 xmldir=ARGV[0]
 
-output=`ruby find-type.rb Unit #{xmldir}`
-
-# whack the newlines, rejoin with spaces
-#unit_files = output.split(/\s+/).join(' ')
-
-words = output.split(/\s+/)
-unit_file_count = words.length()
-unit_files = words.join(' ')
-
-output=`ruby find-type.rb Equipment #{xmldir}`
-words = output.split(/\s+/)
-equip_file_count = words.length()
-equip_files = words.join(' ')
+#output=`ruby find-type.rb Unit #{xmldir}`
+#words = output.split(/\s+/)
+#unit_file_count = words.length()
+#unit_files = words.join(' ')
+#
+#output=`ruby find-type.rb Equipment #{xmldir}`
+#words = output.split(/\s+/)
+#equip_file_count = words.length()
+#equip_files = words.join(' ')
 
 
-puts "processing #{unit_file_count} Unit files"
-`ruby unit-xml2csv.rb #{unit_files} > all-units.csv`
+puts "processing Unit files"
+`ruby find-type.rb Unit #{xmldir} | ruby unit-xml2csv.rb > all-units.csv`
 
-puts "processing #{equip_file_count} Equipment files"
-`ruby equip-xml2csv.rb #{equip_files} > all-equip.csv`
+puts "processing Equipment files"
+`ruby find-type.rb Equipment #{xmldir} | ruby equip-xml2csv.rb > all-equip.csv`
 
 puts "processing Relationships"
 `ruby rel-xml2csv.rb #{unit_files} #{equip_files} > all-relationships.csv`
