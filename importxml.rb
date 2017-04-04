0c0127d0cf2b9ad187df8322c4fc4f7f829e4482
Patrick M. Ryan <pmryan@us.ibm.com>
2017-04-04 16:28:15 -0400

    rewrote to prompt for vcab file name

--- importxml.rb
+++ importxml.rb
@@ -1,19 +1,18 @@
+require 'fileutils'
 
-if (ARGV.length != 1)
-  puts "usage: script directory"
+print "enter name of cab file > "
+line = gets
+cabfile = line.chomp()
+
+xmldir = "xmldir"
+unless File.directory?(xmldir)
+  FileUtils.mkdir_p(xmldir)
 end
 
-xmldir=ARGV[0]
+cmd = "7z x -o#{xmldir} -aoa -bd #{cabfile}"
 
-#output=`ruby find-type.rb Unit #{xmldir}`
-#words = output.split(/\s+/)
-#unit_file_count = words.length()
-#unit_files = words.join(' ')
-#
-#output=`ruby find-type.rb Equipment #{xmldir}`
-#words = output.split(/\s+/)
-#equip_file_count = words.length()
-#equip_files = words.join(' ')
+# run the command
+`#{cmd}`
 
 rand = $$
 units_file="units.#{rand}.txt"
@@ -22,16 +21,22 @@ equip_file="equip.#{rand}.txt"
 `ruby find-type.rb Unit #{xmldir} > #{units_file}`
 `ruby find-type.rb Equipment #{xmldir} > #{equip_file}`
 
+csvname = 'all-units.csv'
 puts "processing Unit files"
-`ruby unit-xml2csv.rb  < #{units_file} > all-units.csv`
+`ruby unit-xml2csv.rb  < #{units_file} > #{csvname}`
+puts "created #{csvname}"
 
+csvname = 'all-equip.csv'
 puts "processing Equipment files"
-`ruby equip-xml2csv.rb < #{equip_file} > all-equip.csv`
+`ruby equip-xml2csv.rb < #{equip_file} > #{csvname}`
+puts "created #{csvname}"
 
+csvname = 'all-relationships.csv'
 puts "processing Relationships"
 # processing Units creates the file. then process Equipment and append the data w/o headers
-`ruby rel-xml2csv.rb < #{units_file}  > all-relationships.csv`
-`ruby rel-xml2csv.rb --noheaders < #{equip_file} >> all-relationships.csv`
+`ruby rel-xml2csv.rb < #{units_file}  > #{csvname}`
+`ruby rel-xml2csv.rb --noheaders < #{equip_file} >> #{csvname}`
+puts "created #{csvname}"
 
 # delete temp files
 File.delete(units_file)
