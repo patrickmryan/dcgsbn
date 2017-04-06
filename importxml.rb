5982ac38ebf7bf8e52d76554ebd5a5d8bf4571c8
Patrick M. Ryan <pmryan@us.ibm.com>
2017-04-06 13:17:36 -0400

    simplified import

--- importxml.rb
+++ importxml.rb
@@ -4,7 +4,18 @@ print "enter name of cab file > "
 line = gets
 cabfile = line.chomp()
 
-xmldir = "xmldir"
+
+home = ENV['userprofile']
+
+desktop = "#{home}/Desktop"
+target = "#{desktop}/DCGS-files"
+scripts = "#{home}/scripts"
+
+unless File.directory?(target)
+        FileUtils.mkdir_p(target)
+end
+
+xmldir = "#{target}/xmldir"
 unless File.directory?(xmldir)
   FileUtils.mkdir_p(xmldir)
 end
@@ -15,29 +26,35 @@ cmd = "7z x -o#{xmldir} -aoa -bd #{cabfile}"
 `#{cmd}`
 
 rand = $$
-units_file="units.#{rand}.txt"
-equip_file="equip.#{rand}.txt"
+units_file="#{target}/units.#{rand}.txt"
+equip_file="#{target}/equip.#{rand}.txt"
 
-`ruby find-type.rb Unit #{xmldir} > #{units_file}`
-`ruby find-type.rb Equipment #{xmldir} > #{equip_file}`
+`ruby #{scripts}/find-type.rb Unit #{xmldir} > #{units_file}`
+`ruby #{scripts}/find-type.rb Equipment #{xmldir} > #{equip_file}`
 
-csvname = 'all-units.csv'
+csvname = "#{target}/all-units.csv"
 puts "processing Unit files"
-`ruby unit-xml2csv.rb  < #{units_file} > #{csvname}`
+`ruby #{scripts}/unit-xml2csv.rb  < #{units_file} > #{csvname}`
 puts "created #{csvname}"
 
-csvname = 'all-equip.csv'
+csvname = "#{target}/all-equip.csv"
 puts "processing Equipment files"
-`ruby equip-xml2csv.rb < #{equip_file} > #{csvname}`
+`ruby #{scripts}/equip-xml2csv.rb < #{equip_file} > #{csvname}`
 puts "created #{csvname}"
 
-csvname = 'all-relationships.csv'
+csvname = "#{target}/all-relationships.csv"
 puts "processing Relationships"
 # processing Units creates the file. then process Equipment and append the data w/o headers
-`ruby rel-xml2csv.rb < #{units_file}  > #{csvname}`
-`ruby rel-xml2csv.rb --noheaders < #{equip_file} >> #{csvname}`
+`ruby #{scripts}/rel-xml2csv.rb < #{units_file}  > #{csvname}`
+`ruby #{scripts}/rel-xml2csv.rb --noheaders < #{equip_file} >> #{csvname}`
 puts "created #{csvname}"
 
 # delete temp files
 File.delete(units_file)
 File.delete(equip_file)
+
+puts
+puts "CSV files are located in #{target}."
+puts 'Hit ENTER to finish.'
+gets
+
