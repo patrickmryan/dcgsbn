98d4becbe8f05d0545fcd2131b8cec2a91ccb5d4
Patrick M. Ryan <pmryan@us.ibm.com>
2017-04-07 15:06:15 -0400

    cleaned out junk

--- csv_weapons.rb
+++ csv_weapons.rb
@@ -24,15 +24,12 @@ end
 arr_of_arrs = CSV.read(ARGV[0])
 output_csv = CSV($stdout, force_quotes: false)
 
-#output_csv << arr_of_arrs[0]  # write out headers
-
 output_csv << headers + [weapon_name]
 
 n = 1
 while (n < arr_of_arrs.length)
   row = arr_of_arrs[n]
 
-  #puts "row #{n}"
   hash = Hash.new('')
   m = 0
   weapons = []
@@ -57,33 +54,6 @@ while (n < arr_of_arrs.length)
     output_csv << row + [w]
   end
 
-  #output_csv << row
-
   n = n+1
 end
 exit
-
-#CSV.foreach(ARGV[0]) do |row|
-#   puts row.to_s
-#end
-#exit
-
-opts = Hash.new()
-opts[:headers] = :first_row
-#input_csv = CSV.new($stdin, opts)
-#puts "opened input"
-
-CSV($stdin) do |input_csv|
-  input_csv.each do |row|
-    puts row.to_s
-  end
-end
-exit
-
-output_csv = CSV($stdout, force_quotes: false)
-puts "opened output"
-
-input_csv.each do | row |
-  output_csv << row
-
-end
