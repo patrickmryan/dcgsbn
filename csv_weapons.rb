2221f077ce3f4628a0736da53da2554d27440e9d
Patrick M. Ryan <pmryan@us.ibm.com>
2017-04-07 14:14:36 -0400

    working version

--- csv_weapons.rb
+++ csv_weapons.rb
@@ -10,10 +10,10 @@ headers = ['ID',
   'radar',
   'unit',
   'DATE',
-  'DATE_FACET_ID' 
+  'DATE_FACET_ID'
   #,'Weapon Name'
-  ]
-  
+]
+
 weapon_name = 'Weapon Name'
 
 if (ARGV.length < 1)
@@ -22,40 +22,51 @@ if (ARGV.length < 1)
 end
 
 arr_of_arrs = CSV.read(ARGV[0])
-output_csv = CSV($stdout, force_quotes: false)  
+output_csv = CSV($stdout, force_quotes: false)
 
 #output_csv << arr_of_arrs[0]  # write out headers
 
 output_csv << headers + [weapon_name]
 
 n = 1
-while (n < arr_of_arrs.length) 
+while (n < arr_of_arrs.length)
   row = arr_of_arrs[n]
-  
-  puts "row #{n}"
+
+  #puts "row #{n}"
   hash = Hash.new('')
   m = 0
+  weapons = []
+
   headers.each do | h |
     # this code assumes that the columns are in the sequence of the headers above
+
     hash[h] = (row[m] ? row[m] : '')
-    puts "#{h} -> " + hash[h]
-    
+
     m=m+1
   end
-  
-  output_csv << row
-  
+
+  [ 'ifv', 'mbt', 'mortar',  'radar' ].each do |w|
+    value = hash[w]  # is there a value in this column?
+    if (value)
+      weapons.concat(value.split(/\s*,\s*/))   # append all the found weapons
+    end
+  end
+
+  # emit a row for every weapon found
+  weapons.each do |w|
+    output_csv << row + [w]
+  end
+
+  #output_csv << row
+
   n = n+1
 end
 exit
-
 
 #CSV.foreach(ARGV[0]) do |row|
 #   puts row.to_s
 #end
 #exit
-
-
 
 opts = Hash.new()
 opts[:headers] = :first_row
@@ -68,12 +79,11 @@ CSV($stdin) do |input_csv|
   end
 end
 exit
-  
-  
-output_csv = CSV($stdout, force_quotes: false)  
+
+output_csv = CSV($stdout, force_quotes: false)
 puts "opened output"
 
 input_csv.each do | row |
   output_csv << row
-  
+
 end
